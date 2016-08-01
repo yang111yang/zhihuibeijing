5012993c65ed5f167dd772b03d163f43814cd7ea
yang111yang <you@example.com>
2016-08-01 14:48:07 +0800

    增加了主页面的侧边栏

--- src/com/itheima/zhuhuibeijing/GuideActivity.java
+++ src/com/itheima/zhuhuibeijing/GuideActivity.java
@@ -3,7 +3,6 @@ package com.itheima.zhuhuibeijing;
 import java.util.ArrayList;
 
 import android.app.Activity;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.PagerAdapter;
--- src/com/itheima/zhuhuibeijing/MainActivity.java
+++ src/com/itheima/zhuhuibeijing/MainActivity.java
@@ -1,19 +1,33 @@
 package com.itheima.zhuhuibeijing;
 
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+import com.jeremyfeinstein.slidingmenu.lib.app.SlidingFragmentActivity;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Window;
-
-public class MainActivity extends Activity {
+/**
+ * 主页面
+ * @author 刘建阳
+ * @date 创建时间：2016-8-1 下午2:28:21
+ */
+public class MainActivity extends SlidingFragmentActivity {
 	
 	
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		
 		setContentView(R.layout.activity_main);
+		
+		setBehindContentView(R.layout.left_menu);
+		
+		SlidingMenu slidingMenu = getSlidingMenu();
+		slidingMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+		slidingMenu.setBehindOffset(200);
+		
 	}
 
 }
