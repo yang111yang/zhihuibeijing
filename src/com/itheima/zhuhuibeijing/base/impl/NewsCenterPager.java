3f71f5f56f99ab987733aab7c4e89818d8e8968c
yang111yang <you@example.com>
2016-08-02 10:14:47 +0800

    使用XUtils开源框架来向服务器请求数据

--- src/com/itheima/zhuhuibeijing/base/impl/NewsCenterPager.java
+++ src/com/itheima/zhuhuibeijing/base/impl/NewsCenterPager.java
@@ -5,10 +5,19 @@ import android.graphics.Color;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.itheima.zhuhuibeijing.base.BasePager;
+import com.itheima.zhuhuibeijing.global.GlobalConstants;
+import com.lidroid.xutils.HttpUtils;
+import com.lidroid.xutils.exception.HttpException;
+import com.lidroid.xutils.http.ResponseInfo;
+import com.lidroid.xutils.http.callback.RequestCallBack;
+import com.lidroid.xutils.http.client.HttpRequest.HttpMethod;
+
 /**
  * 新闻中心
+ * 
  * @author 刘建阳
  * @date 2016-8-1下午10:52:58
  */
@@ -17,23 +26,51 @@ public class NewsCenterPager extends BasePager {
 	public NewsCenterPager(Activity activity) {
 		super(activity);
 	}
-	
+
 	@Override
 	public void initData() {
-		//要给帧布局填充布局对象
+		// 要给帧布局填充布局对象
 		TextView view = new TextView(mActivity);
 		view.setText("新闻中心");
 		view.setTextColor(Color.RED);
 		view.setTextSize(22);
 		view.setGravity(Gravity.CENTER);
-		
+
 		flContent.addView(view);
-		
-		//修改页面标题
+
+		// 修改页面标题
 		tvTitle.setText("新闻");
-		
-		//显示菜单按钮
+
+		// 显示菜单按钮
 		btnMenu.setVisibility(View.VISIBLE);
+
+		// 请求服务器，获取数据
+		// 开源框架：XUtils
+		getDataFromServer();
+	}
+
+	/**
+	 * 从服务器获取数据
+	 */
+	private void getDataFromServer() {
+		HttpUtils utils = new HttpUtils();
+		utils.send(HttpMethod.GET, GlobalConstants.CATEGORY_URL,
+				new RequestCallBack<String>() {
+
+					@Override
+					public void onSuccess(ResponseInfo<String> responseInfo) {
+						// 请求成功
+						String result = responseInfo.result;
+						System.out.println("服务器返回的结果：" + result);
+					}
+
+					@Override
+					public void onFailure(HttpException error, String msg) {
+						// 请求失败
+						error.printStackTrace();
+						Toast.makeText(mActivity, msg, Toast.LENGTH_SHORT).show();
+					}
+				});
 
 	}
 
---
+++ src/com/itheima/zhuhuibeijing/global/GlobalConstants.java
@@ -0,0 +1,8 @@
+package com.itheima.zhuhuibeijing.global;
+
+public class GlobalConstants {
+
+	public static final String SERVER_URL = "http://10.0.2.2:8080/zhbj";//服务器主域名. 10.0.2.2表示模拟器访问本机电脑
+	public static final String CATEGORY_URL = SERVER_URL + "/categories.json";//分类信息接口
+	
+}
