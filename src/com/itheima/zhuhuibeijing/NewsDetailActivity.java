7b03c27dc093e594ec4246ae443a4c40c15211ac
yang111yang <liujianyangyang@163.com>
2016-08-05 22:19:50 +0800

    添加了社会化分享功能，利用ShareSdk开源库来实现

--- src/com/itheima/zhuhuibeijing/NewsDetailActivity.java
+++ src/com/itheima/zhuhuibeijing/NewsDetailActivity.java
@@ -1,5 +1,9 @@
 package com.itheima.zhuhuibeijing;
 
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.onekeyshare.OnekeyShare;
+import cn.sharesdk.onekeyshare.OnekeyShareTheme;
+
 import com.lidroid.xutils.ViewUtils;
 import com.lidroid.xutils.view.annotation.ViewInject;
 
@@ -10,6 +14,7 @@ import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.Window;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebSettings.TextSize;
@@ -45,6 +50,9 @@ public class NewsDetailActivity extends Activity implements OnClickListener {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		
 		setContentView(R.layout.activity_news_detail);
 		
 		ViewUtils.inject(this);
@@ -125,7 +133,7 @@ public class NewsDetailActivity extends Activity implements OnClickListener {
 			showChooseDialog();
 			break;
 		case R.id.btn_share:
-			
+			showShare();
 			break;
 
 		default:
@@ -133,6 +141,38 @@ public class NewsDetailActivity extends Activity implements OnClickListener {
 		}
 		
 	}
+	
+	private void showShare() {
+		 ShareSDK.initSDK(this);
+		 OnekeyShare oks = new OnekeyShare();
+		 
+		 oks.setTheme(OnekeyShareTheme.CLASSIC);
+		 
+		 //关闭sso授权
+		 oks.disableSSOWhenAuthorize(); 
+
+		// 分享时Notification的图标和文字  2.5.9以后的版本不调用此方法
+		 //oks.setNotification(R.drawable.ic_launcher, getString(R.string.app_name));
+		 // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
+		 oks.setTitle(getString(R.string.ssdk_oks_share));
+		 // titleUrl是标题的网络链接，仅在人人网和QQ空间使用
+		 oks.setTitleUrl("http://sharesdk.cn");
+		 // text是分享文本，所有平台都需要这个字段
+		 oks.setText("我是分享文本");
+		 // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+//		 oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+		 // url仅在微信（包括好友和朋友圈）中使用
+		 oks.setUrl("http://sharesdk.cn");
+		 // comment是我对这条分享的评论，仅在人人网和QQ空间使用
+		 oks.setComment("我是测试评论文本");
+		 // site是分享此内容的网站名称，仅在QQ空间使用
+		 oks.setSite(getString(R.string.app_name));
+		 // siteUrl是分享此内容的网站地址，仅在QQ空间使用
+		 oks.setSiteUrl("http://sharesdk.cn");
+
+		// 启动分享GUI
+		 oks.show(this);
+		 }
 	
 	private int mTempWhich;//记录临时选择的字体的大小(点击确定之前)
 	private int mCurrentWhich = 2;//记录当前选择的字体的大小(点击确定之后)
